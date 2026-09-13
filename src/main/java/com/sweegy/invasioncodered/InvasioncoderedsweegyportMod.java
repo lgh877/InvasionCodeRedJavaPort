6c0cd51860b92768eb9c15985f8d5502c3eee619
lgh877 <lgh877@naver.com>
2026-09-13 22:38:01 +0900

    Cleaned some codes

--- src/main/java/com/sweegy/invasioncodered/InvasioncoderedsweegyportMod.java
+++ src/main/java/com/sweegy/invasioncodered/InvasioncoderedsweegyportMod.java
@@ -1,34 +1,19 @@
 package com.sweegy.invasioncodered;
 
-import net.minecraft.world.entity.raid.Raid;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.LogManager;
-
 import net.minecraftforge.network.simple.SimpleChannel;
 import net.minecraftforge.network.NetworkRegistry;
 import net.minecraftforge.network.NetworkEvent;
-import net.minecraftforge.fml.util.thread.SidedThreadGroups;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.common.MinecraftForge;
 
-import net.minecraft.server.TickTask;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.Queue;
-import java.util.PriorityQueue;
-import java.util.Comparator;
-
-import it.unimi.dsi.fastutil.ints.IntObjectPair;
-import it.unimi.dsi.fastutil.ints.IntObjectImmutablePair;
 
 import com.sweegy.invasioncodered.init.*;
 
@@ -38,8 +23,6 @@ public class InvasioncoderedsweegyportMod {
 	public static final String MODID = "invasioncodered";
 
 	public InvasioncoderedsweegyportMod(FMLJavaModLoadingContext context) {
-		// Start of user code block mod constructor
-		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = context.getModEventBus();
 		InvasioncoderedsweegyportModSounds.REGISTRY.register(bus);
@@ -49,12 +32,8 @@ public class InvasioncoderedsweegyportMod {
 		InvasioncoderedsweegyportModParticleTypes.REGISTRY.register(bus);
 		InvasioncoderedsweegyportModAttributes.REGISTRY.register(bus);
         InvasioncoderedsweegyportModMobEffects.REGISTRY.register(bus);
-		// Start of user code block mod init
-		// End of user code block mod init
 	}
 
-	// Start of user code block mod methods
-	// End of user code block mod methods
 	private static final String PROTOCOL_VERSION = "1";
 	public static final SimpleChannel PACKET_HANDLER = NetworkRegistry.newSimpleChannel(ResourceLocation.fromNamespaceAndPath(MODID, MODID), () -> PROTOCOL_VERSION, PROTOCOL_VERSION::equals, PROTOCOL_VERSION::equals);
 	private static int messageID = 0;
